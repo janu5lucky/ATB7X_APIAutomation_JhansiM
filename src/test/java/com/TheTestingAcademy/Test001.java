862d6eccd94384515be25dc95125905e5d690c50
Jhansim <jhansi.muthyala440@gmail.com>
2025-02-07 23:59:55 +0530

    api

---
+++ src/test/java/com/TheTestingAcademy/Test001.java
@@ -0,0 +1,27 @@
+package com.TheTestingAcademy;
+
+import io.restassured.RestAssured;
+
+public class Test001
+{
+    public static void main(String[] args) {
+
+        System.out.println("Rest Assured Test Case");
+        System.out.println("GET Request Demo");
+
+
+        //Gherkins Syntax---------
+        // given() - url, headers ,  body or payload
+        //when() - http methods - get, post , patch, put , delete
+        // then() - verify the response
+        RestAssured
+                .given().baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/800").log().all()
+                .when()
+                .get()
+                .then()
+                .statusCode(404);
+
+
+    }
+}
